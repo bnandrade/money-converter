c544da0c5108e18a87a6b3186b57df89ac274509
Bruno Andrade <bruno_weusa@hotmail.com>
2022-05-11 00:19:36 -0400

    Realizando os calculos da conversão de moedas

--- app/Http/Livewire/Converter.php
+++ app/Http/Livewire/Converter.php
@@ -2,16 +2,27 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
 class Converter extends Component
 {
 
-    public $payment_rate = '1,45%';
+    public $show_form = true;
 
-    public $destination_currency = '';
-    public $value = '';
-    public $payment = '';
+    public $default_currency = 'BRL';                   // moeda padrão (origem) - sigla
+    public $default_currency_ext = 'Real Brasileiro';   // moeda padrão (origem) - por extenso
+    public $destination_currency = '';                  // moeda destino
+    public $value = '';                                 // valor para conversão
+    public $type_payment = '';                          // tipo de pagamento - boleto/cartão
+    public $ask_value = '';                             // valor de venda da moeda estrangeira
+    public $conversion_rate = '';                       // taxa de conversão - 1% ou 2%
+    public $conversion_rate_value = '';                 // valor da taxa de conversão
+    public $payment_rate = '';                          // taxa de forma de pagamento - 1.45% ou 7.63%
+    public $payment_rate_value = '';                    // valor da taxa de forma de pagamento
+    public $default_value = '';                         // valor utilizado para conversão após as taxas
+    public $destination_value = '';                     // valor comprado em moeda destino
+
 
     public function render()
     {
@@ -21,11 +32,31 @@ class Converter extends Component
 
     public function create()
     {
-        $this->validate([
-            'destination_currency' => 'required',
-            'value' => 'required',
-            'payment' => 'required',
-        ]);
+        $this->validate(
+            [
+                'destination_currency' => 'required',
+                'value' => 'required',
+                'type_payment' => 'required',
+            ],
+            [
+                'destination_currency.required' => 'A moeda destino deve ser selecionada',
+                'value.required' => 'O valor para conversão deve ser informado',
+                'type_payment.required' => 'A forma de pagamento deve ser informada',
+            ]
+        );
+
+        $this->show_form = false;
+        $response = Http::get("https://economia.awesomeapi.com.br/json/last/$this->destination_currency-$this->default_currency")->json();
+
+        $this->ask_value = (float) $response["$this->destination_currency"."$this->default_currency"]["ask"];
+        $this->conversion_rate = $this->value < 3000 ? 0.02 : 0.01;
+        $this->payment_rate = $this->type_payment == 'Boleto' ? 0.0145 : 0.0763;
+        $this->conversion_rate_value = $this->value * $this->conversion_rate;
+        $this->payment_rate_value = $this->value * $this->payment_rate;
+        $this->default_value = $this->value - $this->conversion_rate_value - $this->payment_rate_value;
+        $this->destination_value = $this->default_value / $this->ask_value;
+
+
 
     }
 }
--- resources/views/livewire/converter.blade.php
+++ resources/views/livewire/converter.blade.php
@@ -5,77 +5,16 @@
 
             <div class="grid grid-cols-3 gap-4 p-6 bg-white border-gray-200 gap-4">
 
-                <div class="col-span-3 text-lg">
-                    Moeda de origem: <span class=" font-bold">Real Brasileiro (BRL)</span>
-                </div>
-
-                    <div class="flex justify-center">
-                        <div class="mb-3 xl:w-96">
-                            <label for="moedadestino" class="form-label inline-block mb-2 text-gray-700">Selecione a
-                                moeda destino</label>
-                            <select id="moedadestino" wire:model="destination_currency" class="form-select appearance-none
-                                          block
-                                          w-full
-                                          px-3
-                                          py-1.5
-                                          text-base
-                                          font-normal
-                                          text-gray-700
-                                          bg-white bg-clip-padding bg-no-repeat
-                                          border border-solid border-gray-300
-                                          rounded
-                                          transition
-                                          ease-in-out
-                                          m-0
-                                          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none">
-                                <option value="USD">Dólar Americano (USD)</option>
-                                <option value="EUR">Euro (EUR)</option>
-                                <option value="GBP">Libra Esterlina (GBP)</option>
-                                <option value="BTC">Bitcoin (BTC)</option>
-                            </select>
-                            @error('destination_currency')
-                            <span class="text-red-600 ml-2">{{ $message }}</span>
-                            @enderror
-                        </div>
+                @if($show_form)
+                    <div class="col-span-3 text-lg">
+                        Moeda de origem: <span class=" font-bold">{{$default_currency_ext}} ({{$default_currency}})</span>
                     </div>
 
-                    <div class="flex justify-center">
-                        <div class="mb-3 xl:w-96">
-                            <label for="valorconversao" class="form-label inline-block mb-2 text-gray-700">Informe o valor para conversão</label>
-                            <input
-                                type="text"
-                                wire:model="value"
-                                class="
-                                    form-control
-                                    block
-                                    w-full
-                                    px-3
-                                    py-1.5
-                                    text-base
-                                    font-normal
-                                    text-gray-700
-                                    bg-white bg-clip-padding
-                                    border border-solid border-gray-300
-                                    rounded
-                                    transition
-                                    ease-in-out
-                                    m-0
-                                    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-                              "
-                                id="valorconversao"
-                                placeholder="Valor para conversão"
-                            />
-                            @error('value')
-                            <span class="text-red-600 ml-2">{{ $message }}</span>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="flex justify-center">
+                        <div class="flex justify-center">
                             <div class="mb-3 xl:w-96">
-                                <label for="formapagto" class="form-label inline-block mb-2 text-gray-700">Selecione a
-                                    forma de pagamento</label>
-                                <select id="formapagto" wire:model="payment" class="form-select appearance-none
+                                <label for="destination_currency" class="form-label inline-block mb-2 text-gray-700">Selecione a
+                                    moeda destino</label>
+                                <select id="destination_currency" wire:model="destination_currency" class="form-select appearance-none
                                               block
                                               w-full
                                               px-3
@@ -90,21 +29,100 @@
                                               ease-in-out
                                               m-0
                                               focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none">
-                                    <option value="boleto">Boleto</option>
-                                    <option value="cartao-credito">Cartão de crédito</option>
+                                    <option value="">Selecione</option>
+                                    <option value="USD">Dólar Americano (USD)</option>
+                                    <option value="AUD">Dólar Australiano (AUD)</option>
+                                    <option value="EUR">Euro (EUR)</option>
+                                    <option value="GBP">Libra Esterlina (GBP)</option>
                                 </select>
-                                @error('payment')
+                                @error('destination_currency')
                                 <span class="text-red-600 ml-2">{{ $message }}</span>
                                 @enderror
                             </div>
                         </div>
 
-                    <div class="col-span-3 text-center">
+                        <div class="flex justify-center">
+                            <div class="mb-3 xl:w-96">
+                                <label for="value" class="form-label inline-block mb-2 text-gray-700">Informe o valor para conversão</label>
+                                <input
+                                    type="number" min="1000.00" max="100000.00" step="0.01"
+                                    wire:model="value"
+                                    class="
+                                        form-control
+                                        block
+                                        w-full
+                                        px-3
+                                        py-1.5
+                                        text-base
+                                        font-normal
+                                        text-gray-700
+                                        bg-white bg-clip-padding
+                                        border border-solid border-gray-300
+                                        rounded
+                                        transition
+                                        ease-in-out
+                                        m-0
+                                        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+                                  "
+                                    id="value"
+                                    placeholder="Valor para conversão"
+                                />
+                                @error('value')
+                                <span class="text-red-600 ml-2">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
 
+                        <div class="flex justify-center">
+                                <div class="mb-3 xl:w-96">
+                                    <label for="type_payment" class="form-label inline-block mb-2 text-gray-700">Selecione a
+                                        forma de pagamento</label>
+                                    <select id="type_payment" wire:model="type_payment" class="form-select appearance-none
+                                                  block
+                                                  w-full
+                                                  px-3
+                                                  py-1.5
+                                                  text-base
+                                                  font-normal
+                                                  text-gray-700
+                                                  bg-white bg-clip-padding bg-no-repeat
+                                                  border border-solid border-gray-300
+                                                  rounded
+                                                  transition
+                                                  ease-in-out
+                                                  m-0
+                                                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none">
+                                        <option value="">Selecione</option>
+                                        <option value="Boleto">Boleto</option>
+                                        <option value="Cartao-credito">Cartão de crédito</option>
+                                    </select>
+                                    @error('type_payment')
+                                    <span class="text-red-600 ml-2">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
 
+                        <div class="col-span-3 text-center">
+                            <button type="submit" class="bg-blue-500 px-4 py-2 text-lg rounded-lg border-gray-300 text-white hover:bg-blue-700 ">Converter moeda</button>
+                        </div>
 
-                        <button type="submit" class="bg-blue-500 px-4 py-2 text-lg rounded-lg border-gray-300 text-white hover:bg-blue-700 ">Converter moeda</button>
+                @endif
+                @if($ask_value)
+                    <div class="col-span-3" >
+                        Moeda de origem: {{$default_currency}}<br>
+                        Moeda de destino: {{$destination_currency}}<br>
+                        Valor para conversão: R$ {{number_format($value, '2',',','.')}} <br>
+                        Forma de pagamento: {{$type_payment}} <br>
+                        Valor da "Moeda de destino" usado para conversão: {{$ask_value}} <br>
+                        Valor comprado em "Moeda de destino": {{number_format($destination_value, '2',',','.')}} <br>
+                        Taxa de pagamento: R$ {{number_format($payment_rate_value, '2',',','.')}} <br>
+                        Taxa de conversão: R$ {{number_format($conversion_rate_value, '2',',','.')}} <br>
+                        Valor utilizado para conversão descontando as taxas: R$ {{number_format($default_value, '2',',','.')}}
                     </div>
+                    <div class="col-span-3" >
+                        <a href="{{route('dashboard')}}">Nova conversão</a>
+                    </div>
+                @endif
             </div>
             </form>
 
